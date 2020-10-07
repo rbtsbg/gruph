4ab6b85f2851c3c3cee9d1782dcf538bce081d3c
Robert S <rbts@posteo.de>
2020-10-07 15:55:20 +0200

    added type annotation

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub fn test_ungraph() {
     let g = UnGraph::<i32, ()>::from_edges(&[(1, 2), (2, 3), (3, 4), (1, 4)]);
 
     // Find the shortest path from `1` to `4` using `1` as the cost for every edge.
-    let node_map = dijkstra(&g, 1.into(), Some(4.into()), |_| 1);
+    let node_map: i32 = dijkstra(&g, 1.into(), Some(4.into()), |_| 1);
     assert_eq!(&1i32, node_map.get(&NodeIndex::new(4)).unwrap());
 }
 
