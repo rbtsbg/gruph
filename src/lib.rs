48cdc12f682e394fe58ad799fd30af76b9d5015c
Robert S <rbts@posteo.de>
2020-10-07 15:50:18 +0200

    test import of dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+use petgraph::algo::dijkstra;
+use petgraph::graph::{NodeIndex, UnGraph};
+
+#[cfg(test)]
+pub fn test_ungraph() {
+    // Create an undirected graph with `i32` nodes and edges with `()` associated data.
+    let g = UnGraph::<i32, ()>::from_edges(&[(1, 2), (2, 3), (3, 4), (1, 4)]);
+
+    // Find the shortest path from `1` to `4` using `1` as the cost for every edge.
+    let node_map = dijkstra(&g, 1.into(), Some(4.into()), |_| 1);
+    assert_eq!(&1i32, node_map.get(&NodeIndex::new(4)).unwrap());
+}
+
 pub fn hello() {
     println!("Hello, world!");
 }
