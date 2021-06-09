f79f9ccc2660c4e6903a8f2d49426d60b134897f
Robert S <rbts@posteo.de>
2021-06-09 15:18:15 +0200

    refactoring: renamed functions, added dead_code and unsued_variable lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,12 @@
-#[cfg(test)]
-mod test {
-    use petgraph::algo::dijkstra;
-    use petgraph::graph::{Graph, NodeIndex, UnGraph};
-
-    // test the dependency.
-    #[test]
-    pub fn test_ungraph() {
-        // Create an undirected graph with `i32` nodes and edges with `()` associated data.
-        let g = UnGraph::<i32, ()>::from_edges(&[(1, 2), (2, 3), (3, 4), (1, 4)]);
-
-        // Find the shortest path from `1` to `4` using `1` as the cost for every edge.
-        let node_map = dijkstra(&g, 1.into(), Some(4.into()), |_| 1);
-        assert_eq!(&1i32, node_map.get(&NodeIndex::new(4)).unwrap());
-    }
-
-    #[test]
-    pub fn test_line_to_graph() {
-        let input = "(ROOT (S (NP (PRP$ My) (NN dog)) (ADVP (RB also)) (VP (VBZ likes) (S (VP (VBG eating) (NP (NN sausage))))) (. .)))";
-        let g = crate::file_to_graph::line_to_graph(input, &'(', &')');
-        assert_eq!(g.node_count(), 22);
-    }
-
-    #[test]
-    pub fn test_file_to_graph() {
-        let path: &str = "./resources/trees.txt";
-        let res = crate::file_to_graph::file_to_graph(path);
-    }
-
-}
-
 mod file_to_graph {
     use petgraph::graph::Graph;
     use std::fs::File;
     use std::io::Error as IOE;
     use std::io::Read;
 
+    #[allow(dead_code)]
     pub fn file_to_graph(path: &str) -> Result<Vec<Graph<String, ()>>, IOE> {
-        let mut res: Vec<Graph<String, ()>> = Vec::new();
+        let res: Vec<Graph<String, ()>> = Vec::new();
 
         let mut file = File::open(path)?;
         let mut content = String::new();
@@ -74,6 +44,38 @@ mod file_to_graph {
     }
 }
 
+#[cfg(test)]
+mod test {
+    use petgraph::algo::dijkstra;
+    use petgraph::graph::{NodeIndex, UnGraph};
+
+    // test the dependency.
+    #[test]
+    pub fn test_ungraph_is_created_from_scratch() {
+        // Create an undirected graph with `i32` nodes and edges with `()` associated data.
+        let g = UnGraph::<i32, ()>::from_edges(&[(1, 2), (2, 3), (3, 4), (1, 4)]);
+
+        // Find the shortest path from `1` to `4` using `1` as the cost for every edge.
+        let node_map = dijkstra(&g, 1.into(), Some(4.into()), |_| 1);
+        assert_eq!(&1i32, node_map.get(&NodeIndex::new(4)).unwrap());
+    }
+
+    #[test]
+    pub fn test_line_conversion_to_graph() {
+        let input = "(ROOT (S (NP (PRP$ My) (NN dog)) (ADVP (RB also)) (VP (VBZ likes) (S (VP (VBG eating) (NP (NN sausage))))) (. .)))";
+        let g = crate::file_to_graph::line_to_graph(input, &'(', &')');
+        assert_eq!(g.node_count(), 22);
+    }
+
+    #[test]
+    pub fn test_file_to_graph() {
+        let path: &str = "./resources/trees.txt";
+        #[allow(unused_variables)]
+        let res = crate::file_to_graph::file_to_graph(path);
+    }
+
+}
+
 pub fn hello() {
     println!("Hello, world!");
-
+}
