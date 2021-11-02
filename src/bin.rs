f84c8a1ab92356fd3be35f5f08beb7dfc77639ad
Robert S <rbts@posteo.de>
2021-11-02 15:44:27 +0100

    refactoring: tidied up bin

--- src/bin.rs
+++ src/bin.rs
@@ -1,16 +1,5 @@
 extern crate gruph;
 
-#[allow(unused_imports)]
-//use gruph::hello;
-use std::collections::HashSet;
-
-#[allow(unused_variables)]
 pub fn main() {
-    let s = "this is a strirng";
-    let separators = ['(', ')'];
-    let res = s.chars().find(|c| !separators.contains(c));
-    match res {
-        Some(c) => println!("{}", c),
-        _ => println!("Error"),
-    }
+    println!("Hello, world!");
 }
