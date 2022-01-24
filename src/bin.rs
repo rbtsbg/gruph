6f509cf87da7ca480c8be12d160efdf36eae7fde
Robert S <rbts@posteo.de>
2022-01-24 16:36:29 +0100

    lazy line reading

--- src/bin.rs
+++ src/bin.rs
@@ -1,13 +1,17 @@
 extern crate gruph;
 
-use std::fs;
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 use std::env;
 
-pub fn main() {
+pub fn main() -> io::Result<()>{
     let args: Vec<String> = env::args().collect();
     let path_file: &String = &args[1];
-    println!("Input file: {}", path_file);  
-
-    let contents = fs::read_to_string(path_file).expect("Something went wrong reading the file.");
-    println!("Contents: {}", contents);
+    let file = File::open(path_file)?;
+    let reader = BufReader::new(file);
+    for line in reader.lines(){
+        println!("{}", line?);
+    }
+    Ok(())
 }
+
