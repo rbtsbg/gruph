35ec2eab2e063ffc5e0043837d5c3307924c5036
Robert S <rbts@posteo.de>
2022-01-24 16:21:49 +0100

    reading in file

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,13 @@
 extern crate gruph;
 
+use std::fs;
+use std::env;
+
 pub fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let path_file: &String = &args[1];
+    println!("Input file: {}", path_file);  
+
+    let contents = fs::read_to_string(path_file).expect("Something went wrong reading the file.");
+    println!("Contents: {}", contents);
 }
