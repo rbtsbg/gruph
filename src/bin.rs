eeb8769fb5e4b1b51534363116f3e8d5e641fcda
Robert S <rbts@posteo.de>
2022-01-25 14:02:27 +0100

    better path handling

--- src/bin.rs
+++ src/bin.rs
@@ -1,12 +1,20 @@
 extern crate gruph;
 
+use std::path::Path;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use std::env;
 
 pub fn main() -> io::Result<()>{
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2{
+       panic!("No path found."); 
+}
     let path_file: &String = &args[1];
+    let path_exists = Path::new(path_file).exists();
+    if !path_exists  {
+        panic!("File {} does not exist.", path_file)
+    }
     let file = File::open(path_file)?;
     let reader = BufReader::new(file);
     for line in reader.lines(){
