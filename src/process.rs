ec2e519b809b72b92464f218459aca3c281d1f7b
Robert S <rbts@posteo.de>
2022-01-28 15:26:27 +0100

    build_tree now populates hashmap

--- src/process.rs
+++ src/process.rs
@@ -1,10 +1,8 @@
 pub mod text {
     use std::collections::HashMap;
-    
-    use std::usize;
     use petgraph::graph::DiGraph;
     use petgraph::graph::NodeIndex;
-    
+    use std::usize;
     /// Gets the indices of the next node label in a stanford formatted string.
     ///
     /// Assumes that the node is properly closed, i.e. ends in node separator or white space.
@@ -34,7 +32,6 @@ pub mod text {
             index_next_separator + index_start_search,
         ));
     }
-    
     /// Builds a graph from a string representation of a tree.  
     ///
     /// Assumes a tree representation where the nodes are separated as specified by the arguments.
@@ -50,13 +47,13 @@ pub mod text {
         tree_in: &str,
         node_separator_start: &char,
         node_separator_end: &char,
-    ) -> Result<(DiGraph<String, ()>, HashMap<String, NodeIndex>), &'static str>{
+    ) -> Result<(DiGraph<String, ()>, HashMap<String, Vec<NodeIndex>>), &'static str> {
         let mut res = DiGraph::<String, ()>::new(); // directed graph
         let mut indices_nodes: Vec<NodeIndex> = Vec::new();
         let mut index_char: usize = 0;
         let chars: Vec<char> = tree_in.chars().collect();
         let separators = [*node_separator_start, *node_separator_end, ' '];
-        let hm = HashMap::new();
+        let mut hm: HashMap<String, Vec<NodeIndex>> = HashMap::new();
         while index_char < tree_in.len() {
             let ch = chars[index_char];
             if ch == *node_separator_start {
@@ -66,7 +63,17 @@ pub mod text {
                 for i in node_index_start..=node_index_end {
                     node_label.push(chars[i]);
                 }
-                indices_nodes.push(res.add_node(node_label.clone()));
+                let node_index = res.add_node(node_label.clone());
+                indices_nodes.push(node_index);
+                if hm.contains_key(&node_label) {
+                    match hm.get_mut(&node_label) {
+                        Some(v) => v.push(node_index),
+                        None => panic!("Could not retrieve vector of node indices."),
+                    }
+                }
+                else {
+                    hm.insert(node_label, vec![node_index]);
+                }
                 index_char = node_index_end + 1;
             } else if ch == *node_separator_end {
                 if indices_nodes.len() > 1 {
@@ -87,34 +94,29 @@ pub mod text {
         return Ok((res, hm));
     }
 
-        
-        #[test]
-        pub fn test_build_tree() {
-            let input = "(ROOT(S(NP(PRP)(NN))(ADVP(RB))(VP(VBZ)(S(VP(VBG)(NP(NN)))))))";
-            let graph = build_tree(&input, &'(', &')');
-            match graph {
-                Ok(_g) => println!("tree generated"), // todo test something meaningful
-                Err(_) => panic!(),
-            }
+    #[test]
+    pub fn test_build_tree() {
+        let input = "(ROOT(S(NP(PRP)(NN))(ADVP(RB))(VP(VBZ)(S(VP(VBG)(NP(NN)))))))";
+        let graph = build_tree(&input, &'(', &')');
+        match graph {
+            Ok(_g) => println!("tree generated"), // todo test something meaningful
+            Err(_) => panic!(),
         }
-        
-        #[test]
-        pub fn test_node_label_indices_correct() {
-            let input = "(ROOT(S(NP(PRP)(NN))(ADVP(RB))(VP(VBZ)(S(VP(VBG)(NP(NN)))))))";
-            let separators = ['(', ')', ' '];
-            let indices = get_next_node_label_indices(&input, 0, &separators);
-            match indices {
-                Ok((i1, i2)) => assert_eq!((i1, i2), (1, 4)),
-                Err(_) => panic!(),
-            }
-            let indices = get_next_node_label_indices(&input, 0, &separators);
-            match indices {
-                Ok((i1, i2)) => assert_eq!((i1, i2), (1, 4)),
-                Err(_) => panic!(),
-            }
-        }
-        
-        
+    }
 
-    
+    #[test]
+    pub fn test_node_label_indices_correct() {
+        let input = "(ROOT(S(NP(PRP)(NN))(ADVP(RB))(VP(VBZ)(S(VP(VBG)(NP(NN)))))))";
+        let separators = ['(', ')', ' '];
+        let indices = get_next_node_label_indices(&input, 0, &separators);
+        match indices {
+            Ok((i1, i2)) => assert_eq!((i1, i2), (1, 4)),
+            Err(_) => panic!(),
+        }
+        let indices = get_next_node_label_indices(&input, 0, &separators);
+        match indices {
+            Ok((i1, i2)) => assert_eq!((i1, i2), (1, 4)),
+            Err(_) => panic!(),
+        }
+    }
 }
